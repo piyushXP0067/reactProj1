6c7710e0828d76ea9584a89c406587a292d7ecf4
piyushXP0067 <114660663+piyushXP0067@users.noreply.github.com>
2024-04-04 11:22:13 +0530

    navigation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , FlatList} from 'react-native';
-import DayListItem from './src/components/core/DayListItem';
+import { StyleSheet, Text, View , FlatList, ActivityIndicator} from 'react-native';
+import DayListItem from '../components/core/DayListItem';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import {AmaticSC_400Regular,AmaticSC_700Bold}from '@expo-google-fonts/amatic-sc';
 
+SplashScreen.preventAutoHideAsync();
 const days = [...Array(24)].map((val, index) => index + 1);
 
-export default function App() {
+export default function HomeScreen() {
+ const [fontsLoaded, fontError] = useFonts(
+    {
+      Inter: Inter_900Black,
+      amatic: AmaticSC_400Regular,
+      amaticBold: AmaticSC_700Bold,
+    }
+  );
+
+useEffect(() => {
+  if (fontsLoaded || fontError){
+     SplashScreen.hideAsync();
+  }
+},[fontsLoaded, fontError]);
+
+if (!fontsLoaded && !fontError){
+  return null;
+}
+
+
+
   return (
     <View style={styles.container}>
 <FlatList
@@ -50,6 +75,7 @@ const styles = StyleSheet.create({
   },
   text:{
     color:'#9b4521',
-    fontSize: 70
+    fontSize: 70,
+    fontFamily: 'amaticBold'
   }
 });
--- src/components/core/DayListItem.tsx
+++ src/components/core/DayListItem.tsx
@@ -1,23 +1,24 @@
-import { Text , View,StyleSheet} from "react-native";
+import { Link } from "expo-router";
+import { Text , View,StyleSheet, Pressable} from "react-native";
 
 type DayListItem = {
     day: number;
 };
 
-export default function DayListItem({day}: DayListItem){
-
-    return ( 
-    <View style={styles.box}>
-        <Text style={styles.text}>{day}</Text>
-    </View>
-      );
-} 
+export default function DayListItem({ day }: DayListItem) {
+    
+    return (
+      <Link href={`/day${day}`} asChild>
+        <Pressable style={styles.box}>
+          <Text style={styles.text}>{day}</Text>
+        </Pressable>
+      </Link>
+    );
+  }
 
 const styles = StyleSheet.create({
     box: {
       backgroundColor: '#f9ede3',
-      // width:100,
-      // height:100,
       flex:1,
       aspectRatio:1,
       borderRadius:20,
@@ -30,7 +31,8 @@ const styles = StyleSheet.create({
     },
     text:{
       color:'#9b4521',
-      fontSize: 70
+      fontSize: 75,
+      fontFamily:'amaticBold',
     }
   });
   
